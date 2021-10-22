38613b82ec54682e3122fec7d0a4f0eb15fb9931
Federica <federicacdoglio@gmail.com>
2021-10-22 20:36:57 -0300

    primeros cambiosssss

--- src/ejecucion/Prueba.java
+++ src/ejecucion/Prueba.java
@@ -5,8 +5,13 @@
  */
 package ejecucion;
 
+import vista.VentanaPrincipal;
+
 public class Prueba {
     public static void main(String[] args) {
-        // TODO code application logic here
+       VentanaPrincipal ventana = new VentanaPrincipal();
+       ventana.setVisible(true);
+       
+       
     }
 }
